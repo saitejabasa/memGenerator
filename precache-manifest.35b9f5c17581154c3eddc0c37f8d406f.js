cb6d478053e873802842787000ef2a19cc55f276
Basa Saiteja <41227236+saitejabasa@users.noreply.github.com>
2020-06-02 13:00:58 +0530

    Add files via upload

---
+++ precache-manifest.35b9f5c17581154c3eddc0c37f8d406f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "191142283a3f2479492b59bd18c5e919",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8607d1e2f6dbebd871e6",
+    "url": "/static/css/2.42ec2a3e.chunk.css"
+  },
+  {
+    "revision": "1767059c9ba64f941853",
+    "url": "/static/css/main.73a88722.chunk.css"
+  },
+  {
+    "revision": "8607d1e2f6dbebd871e6",
+    "url": "/static/js/2.8b5251b7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8b5251b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1767059c9ba64f941853",
+    "url": "/static/js/main.fbc47530.chunk.js"
+  },
+  {
+    "revision": "4377fcd7cc437b367997",
+    "url": "/static/js/runtime-main.c64051e1.js"
+  }
+]);
